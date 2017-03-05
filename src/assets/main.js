ce9fcb5bf711311c2af1be651569b8f1d8808d23
Narek Avagyan <narek_avagyan@inbox.ru>
2017-03-05 11:23:54 -0800

    setHiddenFunction

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,3 +7,11 @@ function guess() {
 }
 
 //implement new functions here
+
+function setHiddenFields(){
+  answer = Math.floor(Math.random() * (9999 - 0)) + 0;
+  return answer;
+  while (answer.length < 4){
+    answer.toString()
+  };
+}
